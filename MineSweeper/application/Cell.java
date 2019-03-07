4e0bfed9f5f050c1ed7c581a42d094510ba9bda5
Christian Coplen <cmcoplen@iu.edu>
2019-03-07 12:25:59 -0500

    Comment on methods

--- MineSweeper/application/Cell.java
+++ MineSweeper/application/Cell.java
@@ -18,19 +18,22 @@ class Cell {
 		this.nearbyMines = nearbyMines;
 	}
 	
+	// Setter method for revealing Cells
 	public void revealCell() {
 		this.isRevealed = true;
 	}
 	
+	// setter method for flagging Cells
 	public void flagCell() {
 		this.isFlagged = true;
 	}
 	
+	// Getter method for nearby Cells
 	public int countNearbyMines(Cell currentCell) {
 		int nearMines = currentCell.nearbyMines;
 		return nearMines;
 	}
-	
+	// Getter method checking if Cell is a mine or not
 	public boolean mineCheck(Cell currentCell) {
 		boolean status = currentCell.isMine;
 		return status;
