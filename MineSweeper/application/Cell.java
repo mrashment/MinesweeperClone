c577edf4659cf4b64c24dea8a3c12e76d0799b80
Christian Coplen <cmcoplen@iu.edu>
2019-03-19 11:45:35 -0400

    Create to String method for result of clicking on Cell

--- MineSweeper/application/Cell.java
+++ MineSweeper/application/Cell.java
@@ -27,15 +27,34 @@ class Cell {
 	public void flagCell() {
 		this.isFlagged = true;
 	}
-	
+	// setter method to unflag cell
+	public void unflagCell() {
+		this.isFlagged = false;
+	}
 	// Getter method for nearby Cells
-	public int countNearbyMines(Cell currentCell) {
-		int nearMines = currentCell.nearbyMines;
+	public int countNearbyMines() {
+		int nearMines = this.nearbyMines;
 		return nearMines;
 	}
 	// Getter method checking if Cell is a mine or not
-	public boolean mineCheck(Cell currentCell) {
-		boolean status = currentCell.isMine;
+	public boolean mineCheck() {
+		boolean status = this.isMine;
 		return status;
+	}
+	//Getter method for checking if a cell is flagged
+	public boolean flagCheck() {
+		boolean flagStatus = this.isFlagged;
+		return flagStatus;
+	}
+	// To String Method containing result of the current cell clicked
+	public String toString() {
+		String result = "";
+		if (this.mineCheck()) {
+			result += "*";
+		}
+		if (this.isFlagged) {
+			result += "F";
+		}
+		return result;
 	}
 }
