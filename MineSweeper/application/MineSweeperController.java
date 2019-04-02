5646befbe07b9159db020d8af4b70215a59479f6
Smith, Ashton T <smithast@iu.edu>
2019-04-02 11:38:27 -0400

    UML Diagram added

--- MineSweeper/application/MineSweeperController.java
+++ MineSweeper/application/MineSweeperController.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.IOException;
 
+import javax.print.DocFlavor.URL;
+
 import javafx.event.ActionEvent;
 //import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -13,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -61,19 +64,24 @@ public class MineSweeperController {
 			pressedButtonCol = GridPane.getColumnIndex(pressedButton);
 		}
 		if(flagCheckBox.isSelected()) {
-			if(pressedButton.getText().equals("F")) {
-				pressedButton.setText("");
+			if(pressedButton.getGraphic() != null) {
+				pressedButton.setGraphic(null);
 				game.getCell(pressedButtonCol, pressedButtonRow).unflagCell();
 			} else {
-				pressedButton.setText("F");
+				ImageView flagImg = new ImageView("/flag.png");
+				flagImg.setFitWidth(31);
+				flagImg.setFitHeight(31);
+				pressedButton.setGraphic(flagImg);
 				game.getCell(pressedButtonCol, pressedButtonRow).flagCell();
 			}
-		} else if(pressedButton.getText().equals("F")){
+		} else if(pressedButton.getGraphic() != null){
 			
 		} else {
 			//game.getCellArray()[pressedButtonRow][pressedButtonCol].revealCell();
 			game.revealCell(game.getCell(pressedButtonCol, pressedButtonRow), pressedButtonCol, pressedButtonRow);
-			System.out.println(game.getCell(pressedButtonCol, pressedButtonRow).toString());
+			
+			System.out.println(game.getCell(pressedButtonCol, pressedButtonRow).toString() + " bombs near revealed cell.");
+			
 			this.checkReveal();
 			//game.checkStatus(pressedButtonRow, pressedButtonCol);
 			//System.out.println(pressedButtonRow + ", " + pressedButtonCol + " revealed");
@@ -111,39 +119,52 @@ public class MineSweeperController {
 			for(int j = 0; j < game.getCellArray()[i].length; j++) {
 				Cell thisCell = game.getCell(i, j);
 				Label cellLabel = new Label();
-				cellLabel.setText(thisCell.toString());
-				cellLabel.setFont(Font.getDefault());
-				cellLabel.setPrefSize(81, 31);
-				cellLabel.setTextAlignment(TextAlignment.CENTER);
-				boardPane.add(cellLabel, i, j);
-				switch(thisCell.toString()) {
-				case "*":
-					cellLabel.setTextFill(Color.RED);
-					break;
-				case "1":
-					cellLabel.setTextFill(Color.DARKBLUE);
-					break;
-				case "2":
-					cellLabel.setTextFill(Color.DARKGREEN);
-					break;
-				case "3":
-					cellLabel.setTextFill(Color.DARKRED);
-					break;
-				case "4":
-					cellLabel.setTextFill(Color.BLUEVIOLET);
-					break;
-				case "5":
-					cellLabel.setTextFill(Color.DARKGOLDENROD);
-					break;
-				default:
-					cellLabel.setTextFill(Color.BROWN);
-					break;
+
+				if(!thisCell.mineCheck()) {
+					cellLabel.setText(thisCell.toString());
+					cellLabel.setFont(Font.getDefault());
+					cellLabel.setPrefSize(81, 31);
+					cellLabel.setTextAlignment(TextAlignment.CENTER);
+					boardPane.add(cellLabel, i, j);
+					switch(thisCell.toString()) {
+					case "*":
+						cellLabel.setTextFill(Color.RED);
+						break;
+					case "1":
+						cellLabel.setTextFill(Color.DARKBLUE);
+						break;
+					case "2":
+						cellLabel.setTextFill(Color.DARKGREEN);
+						break;
+					case "3":
+						cellLabel.setTextFill(Color.DARKRED);
+						break;
+					case "4":
+						cellLabel.setTextFill(Color.BLUEVIOLET);
+						break;
+					case "5":
+						cellLabel.setTextFill(Color.DARKGOLDENROD);
+						break;
+					default:
+						cellLabel.setTextFill(Color.BROWN);
+						break;
+					}
+					cellLabel.setStyle("-fx-font-weight: bold");
+					GridPane.setFillWidth(cellLabel, true);
+					cellLabel.setMaxWidth(Double.MAX_VALUE);
+					cellLabel.setAlignment(Pos.CENTER);
+					cellLabel.toBack();
+				} else {
+					ImageView bombImg = new ImageView("/bomb.png");
+					bombImg.setFitHeight(31);
+					bombImg.setFitWidth(31);
+					cellLabel.setGraphic(bombImg);
+					boardPane.add(cellLabel, i, j);
+					GridPane.setFillWidth(cellLabel, true);
+					cellLabel.setMaxWidth(Double.MAX_VALUE);
+					cellLabel.setAlignment(Pos.CENTER);
+					cellLabel.toBack();
 				}
-				cellLabel.setStyle("-fx-font-weight: bold");
-				GridPane.setFillWidth(cellLabel, true);
-				cellLabel.setMaxWidth(Double.MAX_VALUE);
-				cellLabel.setAlignment(Pos.CENTER);
-				cellLabel.toBack();
 			}
 		}
 	}
