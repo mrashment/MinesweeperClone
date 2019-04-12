bc1e83a3bd473e9ea4918230b28c6d8b23312909
ndorenbo <ndorenbo@indiana.edu>
2019-04-12 12:28:39 -0400

    initial CellButton class

---
+++ MineSweeper/application/CellButton.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.scene.control.Button;
+
+public class CellButton extends Button {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private boolean isRevealed;
+	private boolean isMine;
+	private boolean isFlagged;
+	private int nearbyMines;
+	private int x,y;
+
+}
